049d1c88925e7359cbc7b0a06ac7cb43f408fdbb
Miriam <cruz.miriam@uabc.edu.mx>
2020-04-01 19:31:44 -0700

    modificacion

--- src/Test/org/programa/clases/MicroorganismoTest.java
+++ src/Test/org/programa/clases/MicroorganismoTest.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class MicroorganismoTest {
+public class MicroorganismoTest {
+
     private int vidaMax = 500;
     private Microorganismo micro = new Microorganismo("MicroPrueba",vidaMax);
 
